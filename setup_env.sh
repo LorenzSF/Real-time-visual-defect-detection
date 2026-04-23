51c3df85e732eda7e81652261c2d923f4bf494e3
Lorenzo Fresca <lorenzostefano.fresca@student.kuleuven.be>
2026-04-23 13:20:16 +0000

    Upload dataset in scratch

---
+++ setup_env.sh
@@ -0,0 +1,5 @@
+export PATH="/scratch/leuven/381/vsc38124/uv-x86_64-unknown-linux-gnu:$PATH"
+export UV_CACHE_DIR=/scratch/leuven/381/vsc38124/.uv_cache
+export UV_PROJECT_ENVIRONMENT=/scratch/leuven/381/vsc38124/.venv
+export UV_PYTHON_INSTALL_DIR=/scratch/leuven/381/vsc38124/.uv_python
+export PYTHONPATH=src
